d71a82bcd434a822fb36ee63ace192d17598717a
mrhid6 <dominic.richardson1@hotmail.co.uk>
2026-02-15 18:42:23 +0000

    Bump version to 0.0.80.

--- utils/mapper/modmap.go
+++ utils/mapper/modmap.go
@@ -1,8 +1,11 @@
 package mapper
 
 import (
+	"time"
+
 	models "github.com/SatisfactoryServerManager/ssmcloud-resources/models"
 	pb "github.com/SatisfactoryServerManager/ssmcloud-resources/proto/generated/models"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func MapModToProto(mod *models.ModSchema) *pb.Mod {
@@ -16,6 +19,10 @@ func MapModToProto(mod *models.ModSchema) *pb.Mod {
 	return &pb.Mod{
 		Id:           mod.ID.Hex(),
 		ModId:        mod.ModID,
+		ModName:      mod.ModName,
+		Hidden:       mod.Hidden,
+		LogoUrl:      mod.LogoURL,
+		Downloads:    int32(mod.Downloads),
 		ModReference: mod.ModReference,
 		Versions:     pbModVersions,
 	}
@@ -29,9 +36,19 @@ func MapModVersionToProto(modVersion *models.ModVersion) *pb.ModVersion {
 		pbModVersionTargets = append(pbModVersionTargets, MapModVersionTargetToProto(&modVersion.Targets[i]))
 	}
 
+	createdAtTime, _ := time.Parse(time.RFC3339, modVersion.CreatedAt)
+
+	pbModVersionDependencies := make([]*pb.ModVersionDependency, 0, len(modVersion.Dependencies))
+
+	for i := range modVersion.Dependencies {
+		pbModVersionDependencies = append(pbModVersionDependencies, MapModVersionDependencyToProto(modVersion.Dependencies[i]))
+	}
+
 	return &pb.ModVersion{
-		Version: modVersion.Version,
-		Targets: pbModVersionTargets,
+		Version:      modVersion.Version,
+		Targets:      pbModVersionTargets,
+		Dependencies: pbModVersionDependencies,
+		CreatedAt:    timestamppb.New(createdAtTime),
 	}
 }
 
@@ -39,5 +56,13 @@ func MapModVersionTargetToProto(modVersionTarget *models.ModVersionTarget) *pb.M
 	return &pb.ModVersionTarget{
 		TargetName: modVersionTarget.TargetName,
 		Link:       modVersionTarget.Link,
+	}
+}
+
+func MapModVersionDependencyToProto(dependency models.ModVersionDependency) *pb.ModVersionDependency {
+	return &pb.ModVersionDependency{
+		ModReference: dependency.ModReference,
+		Condition:    dependency.Condition,
+		Optional:     dependency.Optional,
 	}
 }
